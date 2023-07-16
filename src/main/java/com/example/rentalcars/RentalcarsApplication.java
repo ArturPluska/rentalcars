c72ba51b7b6d01969f27fdec29bafd185fbd7572
ArturPluska <artur.pluska@outlook.com>
2023-07-16 11:12:17 +0200

    Adding car class.

--- src/main/java/com/example/rentalcars/RentalcarsApplication.java
+++ src/main/java/com/example/rentalcars/RentalcarsApplication.java
@@ -2,6 +2,7 @@ package com.example.rentalcars;
 
 import com.example.rentalcars.cars.BodyType;
 import com.example.rentalcars.cars.CarModel;
+import com.example.rentalcars.places.Place;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -17,8 +18,19 @@ public class RentalcarsApplication {
         sCar.setColor("blue");
         sCar.setBodyType(BodyType.WAGON);
 
+        Place fPlace = new Place("Warszafka", "Warsaw", "Listopada", "123", 50);
+        Place sPlace = new Place();
+        sPlace.setName("Poznaniak");
+        sPlace.setCity("Poznan");
+        sPlace.setStreet("Grunwaldzka");
+        sPlace.setBuildingNumber("34");
+        sPlace.setAvailableCarsNumber(23);
+
         System.out.println(fCar.getPlateNumber() + " " + fCar.getBrand() + " " + fCar.getModel() + " " + fCar.getColor() + " " + fCar.bodyType);
         System.out.println(sCar.getPlateNumber() + " " + sCar.getBrand() + " " + sCar.getModel() + " " + sCar.getColor() + " " + sCar.bodyType);
+        System.out.println();
+        System.out.println(fPlace.getName() + " " + fPlace.getCity() + " " + fPlace.getStreet() + " " + fPlace.getBuildingNumber() + " " + fPlace.getAvailableCarsNumber());
+        System.out.println(sPlace.getName() + " " + sPlace.getCity() + " " + sPlace.getStreet() + " " + sPlace.getBuildingNumber() + " " + sPlace.getAvailableCarsNumber());
 
     }
 
